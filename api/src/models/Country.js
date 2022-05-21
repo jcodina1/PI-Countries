338b71fe3ae3358f110117a9ebd655086cf442ae
jcodina1 <codina844@gmail.com>
2022-05-21 13:52:39 -0500

    models database check

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -4,9 +4,38 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
+    id:{
+      type:DataTypes.STRING(3),
+      allowNull:false
+    },
     name: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    img:{
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    continent:{
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    capital:{
+      type:DataTypes.STRING,
+      allowNull:false
+    },
+    subRegion:{
+      type:DataTypes.STRING,
+      
+    },
+    area:{
+      type:DataTypes.STRING,
+      
+    },
+    poblacion:{
+      type:DataTypes.STRING,
+      
+    }
+    
   });
 };
